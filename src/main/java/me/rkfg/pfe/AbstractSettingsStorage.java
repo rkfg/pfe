d6d952dc27594360ec401c8cd8026498505353cb
rkfg <rkfg@rkfg.me>
2016-01-31 13:26:47 +0300

    Abstract over settings storage.

---
+++ src/main/java/me/rkfg/pfe/AbstractSettingsStorage.java
@@ -0,0 +1,50 @@
+package me.rkfg.pfe;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Properties;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public abstract class AbstractSettingsStorage {
+    protected Properties properties = new Properties();
+    protected Logger log = LoggerFactory.getLogger(getClass());
+    private String iniFile;
+
+    protected AbstractSettingsStorage(Class<?> jarClass, String filename) {
+        iniFile = new File(getJarDirectory(jarClass), filename).getAbsolutePath();
+        try {
+            properties.load(new InputStreamReader(new FileInputStream(new File(iniFile)), StandardCharsets.UTF_8));
+        } catch (IOException e) {
+            log.info("{} not found, will be created later.", iniFile);
+        }
+    }
+
+    protected void storeProperties() {
+        try {
+            properties.store(new OutputStreamWriter(new FileOutputStream(new File(iniFile)), StandardCharsets.UTF_8), "");
+        } catch (IOException e) {
+            log.info("{} can't be saved, path info will always be default.", iniFile);
+        }
+    }
+
+    public static String getJarDirectory(Class<?> clazz) {
+        try {
+            return new File(URLDecoder.decode(clazz.getProtectionDomain().getCodeSource().getLocation().getPath(),
+                    "utf-8"))
+                    .getParent();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            return ".";
+        }
+    }
+
+}
--- src/main/java/me/rkfg/pfe/SettingsStorage.java
+++ src/main/java/me/rkfg/pfe/SettingsStorage.java
@@ -1,36 +1,19 @@
 package me.rkfg.pfe;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.net.URLDecoder;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
-public class SettingsStorage {
+public class SettingsStorage extends AbstractSettingsStorage {
 
-    private Properties properties = new Properties();
     private Collection<String> trackers;
     private boolean dht;
     private int seedRatio;
     private long seedingTimeout;
     private boolean seedAfterDownload;
 
-    public SettingsStorage() {
-        File settingsFile = null;
-        String settingsFilename = System.getProperty("pfe.settings");
-        if (settingsFilename == null) {
-            settingsFilename = new File(getJarDirectory(), "pfe_settings.ini").getAbsolutePath();
-        }
-        settingsFile = new File(settingsFilename);
-        try {
-            properties.load(new FileInputStream(settingsFile));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public SettingsStorage(Class<?> jarClass) {
+        super(jarClass, System.getProperty("pfe.settings", "pfe_settings.ini"));
         dht = Boolean.valueOf(properties.getProperty("enable_dht", "false"));
         seedRatio = Integer.valueOf(properties.getProperty("seeding_ratio", "3"));
         trackers = Arrays.asList(properties.getProperty("trackers", "").split("\\|"));
@@ -58,15 +41,6 @@ public class SettingsStorage {
 
     public boolean isSeedAfterDownload() {
         return seedAfterDownload;
-    }
-
-    public static String getJarDirectory() {
-        try {
-            return new File(URLDecoder.decode(SettingsStorage.class.getProtectionDomain().getCodeSource().getLocation().getPath(), "utf-8")).getParent();
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-            return ".";
-        }
     }
 
 }
